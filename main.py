dd36eabb48b92e4794ad89787c8c3b41d7c7dd29
viral rupani <viralrupani12017@gmail.com>
2022-08-30 15:07:26 +0530

    fix: fix crashing bug

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, redirect, url_for
 from dotenv import load_dotenv
+import string
 import requests
 import os
 
@@ -9,7 +10,6 @@ load_dotenv()
 
 API_KEY = os.environ.get('API_KEY')
 
-
 @app.route("/", methods=["GET", "POST"])
 def home_page():
     if request.method == "POST":
@@ -17,13 +17,13 @@ def home_page():
     return render_template("index.html")
 
 
-@app.route("/<string:word>")
+@app.route("/word/<string:word>")
 def word_page(word):
     headers = {
         "Authorization": f'Token {API_KEY}'
     }
     with requests.get(f'https://owlbot.info/api/v4/dictionary/{word}', headers=headers) as response:
-        if response.status_code != 200:
+        if response.status_code != 200 or word[0].lower() not in list(string.ascii_lowercase):
             return render_template('not_found.html', error=response.status_code)
     return render_template('word.html', word=response.json())
 
